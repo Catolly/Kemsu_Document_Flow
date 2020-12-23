84eea1ec2c01efe5f244b8cd1a3454cefeed3358
ASK3T <45347427+ASK3T@users.noreply.github.com>
2020-12-23 21:54:13 +0700

    Клиентская часть завернута в отдельную папку

---
+++ client/pages/TestPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <section id="container">
+    <registration-layout></registration-layout>
+  </section>
+</template>
+
+<script>
+
+import RegistrationLayout from '~/layouts/RegistrationLayout'
+
+export default {
+  components: {
+    RegistrationLayout
+  }
+}
+</script>
+
+<style>
+
+</style>
+
