745762f3c3fed233565502de60885f7c39b6cfa3
ASK3T <45347427+ASK3T@users.noreply.github.com>
2021-01-06 18:00:07 +0700

    feat(form, login, signup): Вынести повторяющиеся стили в слой FormLayout
    В LoginLayout и SignupLayout повторяются 50 строк стилей, а таких Layout'ов будет около 5. Поэтому стили проще вынести в отдельный FormLayout.
    
    Размер input не выносится в FormLayout, т.к. в FormLayout он не работает

---
+++ client/layouts/FormLayout.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormLayout.vue',
+}
+</script>
+
+<style lang="less" scoped>
+@import '~/styles/index.less';
+
+#container {
+  position: relative;
+}
+
+form {
+  width: 516px;
+}
+
+h1 {
+  text-align: center;
+}
+
+#form-inner {
+  margin-top: 48px;
+
+  & p {
+    margin-top: 8px;
+  }
+}
+
+input {
+  font-size: @fz-large;
+  line-height: 160%;
+}
+
+button {
+  height: 70px;
+  width: 100%;
+  margin-top: 10px;
+
+  color: @blue;
+  border: 1px solid @blue;
+
+  &:first-child {
+    margin-top: 32px;
+  }
+  &.filled {
+    background-color: @blue;
+    border: none;
+    color: @theme-color;
+  }
+}
+
+p {
+  margin-top: 18px;
+}
+
+// input {
+//   height: 70px;
+// }
+
+</style>
+
--- client/layouts/SignupLayout.vue
+++ client/layouts/SignupLayout.vue
@@ -1,56 +1,60 @@
 <template>
   <div id="container">
     <div id="signup-wrapper">
-      <form @submit="checkForm()" id="signup-form"> 
-        <h1>Регистрация</h1>
-        <div id="form-inner">
-          <v-input
-          v-model.trim="form.username"
-          :class="$v.form.username.$error ? 'is-invalid' : ''"
-          id="username"
-          type="text"
-          placeholder="Ф.И.О."
-          required></v-input>
-          <v-input
-          v-model.trim="form.email"
-          :class="$v.form.email.$error ? 'is-invalid' : ''"
-          id="email"
-          type="email"
-          placeholder="Email"
-          required></v-input>
-          <v-input
-          v-model.trim="form.password"
-          :class="$v.form.password.$error ? 'is-invalid' : ''"
-          id="password"
-          type="password"
-          placeholder="Пароль"
-          required></v-input>
-          <div class="buttons-wrapper">
-            <button
-            class="filled"
-            id="signup">
-              Зарегистрироваться
-            </button>
-            <button
-            id="login">
-              Войти
-            </button>
+      <form-layout>
+        <form @submit="checkForm()" id="signup-form"> 
+          <h1>Регистрация</h1>
+          <div id="form-inner">
+            <v-input
+            v-model.trim="form.username"
+            :class="$v.form.username.$error ? 'is-invalid' : ''"
+            id="username"
+            type="text"
+            placeholder="Ф.И.О."
+            required></v-input>
+            <v-input
+            v-model.trim="form.email"
+            :class="$v.form.email.$error ? 'is-invalid' : ''"
+            id="email"
+            type="email"
+            placeholder="Email"
+            required></v-input>
+            <v-input
+            v-model.trim="form.password"
+            :class="$v.form.password.$error ? 'is-invalid' : ''"
+            id="password"
+            type="password"
+            placeholder="Пароль"
+            required></v-input>
+            <div class="buttons-wrapper">
+              <button
+              class="filled"
+              id="signup">
+                Зарегистрироваться
+              </button>
+              <button
+              id="login">
+                Войти
+              </button>
+            </div>
           </div>
-        </div>
-        <p>Кто-то зарегистрировался под вашим именем? Обратитесь к <NuxtLink to="#">администрации</NuxtLink></p>
-      </form>
+          <p>Кто-то зарегистрировался под вашим именем? Обратитесь к <NuxtLink to="#">администрации</NuxtLink></p>
+        </form>
+      </form-layout>
     </div>
   </div>
 </template>
 
 <script>
 import VInput from '~/components/VInput'
+import FormLayout from '~/layouts/FormLayout'
 import { required, minLength, email } from 'vuelidate/lib/validators'
 
 export default {
   name: 'SignupLayout.vue',
   components: {
-    VInput
+    VInput,
+    FormLayout
   },
   data() {
     return {
@@ -94,45 +98,8 @@ export default {
   .flex(center, center);
 }
 
-#signup-form {
-  width: 516px;
-}
-
-h1 {
-  text-align: center;
-}
-
-#form-inner {
-  margin-top: 48px;
-}
-
 input {
   height: 70px;
-
-  font-size: @fz-large;
-  line-height: 160%;
-}
-
-button {
-  height: 70px;
-  width: 100%;
-  margin-top: 4px;
-
-  color: @blue;
-  border: 1px solid @blue;
-
-  &:first-child {
-    margin-top: 32px;
-  }
-  &.filled {
-    background-color: @blue;
-    border: none;
-    color: @theme-color;
-  }
-}
-
-p {
-  margin-top: 18px;
 }
 
 </style>
