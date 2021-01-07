b375b17fef2cf9d8132e60d559ccb292e2c4bab2
gumart <mihail_lyukshin@mail.ru>
2021-01-07 15:03:47 +0700

    Build(data base): Подключение базы данных PostgreSQL

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydoclocaldb',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
