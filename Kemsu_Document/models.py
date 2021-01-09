3d214208051c7d44aeb1a133a1aed629e736edd3
gumart <mihail_lyukshin@mail.ru>
2021-01-09 19:47:40 +0700

    Build(data base): Измененны связи в базе данных

--- Kemsu_Document/models.py
+++ Kemsu_Document/models.py
@@ -3,40 +3,6 @@ from django.contrib.auth.hashers import (
     check_password, is_password_usable, make_password,
 )
 
-
-class Student(models.Model):
-    id = models.AutoField(primary_key=True)
-    course = models.IntegerField()
-    direction_of_study = models.CharField(max_length=75)
-    institute = models.CharField(max_length=75)
-    group = models.CharField(max_length=30)
-
-
-class Employee(models.Model):
-    id = models.AutoField(primary_key=True)
-    department_name = models.CharField(max_length=75)
-
-
-class Administrator(models.Model):
-    id = models.AutoField(primary_key=True)
-
-
-class Document(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=75)
-    #point = models.CharField(max_length=75)
-    # data = models.CharField(max_length=75)
-    link_on_Student = models.ForeignKey(Student, on_delete=models.CASCADE)
-
-
-class Point(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=75)
-    description = models.TextField()
-    file = models.FileField()
-    link_on_module = models.ForeignKey(Document, on_delete=models.CASCADE)
-
-
 class Users(models.Model):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=75)
@@ -48,6 +14,37 @@ class Users(models.Model):
     #link_on_Student = models.ForeignKey(Student, on_delete=models.CASCADE)
     #link_on_Employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     #link_on_Administrator = models.ForeignKey(Administrator, on_delete=models.CASCADE)
+
+class Administrator(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+class Employee(models.Model):
+    id = models.AutoField(primary_key=True)
+    department_name = models.CharField(max_length=75)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+class Student(models.Model):
+    id = models.AutoField(primary_key=True)
+    course = models.IntegerField()
+    direction_of_study = models.CharField(max_length=75)
+    institute = models.CharField(max_length=75)
+    group = models.CharField(max_length=30)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+class Document(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=75)
+    #point = models.CharField(max_length=75)
+    # data = models.CharField(max_length=75)
+    link_on_Student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+class Point(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=75)
+    description = models.TextField()
+    file = models.FileField()
+    link_on_module = models.ForeignKey(Document, on_delete=models.CASCADE)
 
 @staticmethod
 def create_user(first_name, last_name, middle_name, e_mail, password, **any_arguments):
@@ -62,3 +59,4 @@ def create_user(first_name, last_name, middle_name, e_mail, password, **any_argu
 @staticmethod
 def set_password(user, raw_password):
     user.password = make_password(raw_password)
+
