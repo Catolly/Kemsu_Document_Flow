a9942c4b0d23030a53c8e0d6f879d38c742c247d
ASK3T <45347427+ASK3T@users.noreply.github.com>
2021-01-13 00:03:39 +0700

    Revert "feat(form, login, signup): Вынести повторяющиеся стили в слой FormLayout"
    This reverts commit 745762f3c3fed233565502de60885f7c39b6cfa3.

--- client/layouts/LoginLayout.vue
+++ client/layouts/LoginLayout.vue
@@ -1,55 +1,51 @@
 <template>
   <div id="container">
     <div id="login-wrapper">
-      <form-layout>
-        <form @submit="checkForm()" id="login-form"> 
-          <h1>Вход</h1>
-          <div id="form-inner">
-            <v-input
-            v-model.trim="form.username"
-            :class="$v.form.username.$error ? 'is-invalid' : ''"
-            id="username"
-            type="text"
-            placeholder="Ф.И.О."
-            required></v-input>
-            <v-input
-            v-model.trim="form.password"
-            :class="$v.form.password.$error ? 'is-invalid' : ''"
-            id="password"
-            type="password"
-            placeholder="Пароль"
-            required></v-input>
-            <p>
-              <NuxtLink to="#">Не помню пароль</NuxtLink>
-            </p>
-            <div class="buttons-wrapper">
-              <button
-              id="login"
-              class="filled">
-                Войти
-              </button>
-              <button
-              id="signup">
-                Зарегистрироваться
-              </button>
-            </div>
+      <form @submit="checkForm()" id="login-form"> 
+        <h1>Вход</h1>
+        <div id="form-inner">
+          <v-input
+          v-model.trim="form.username"
+          :class="$v.form.username.$error ? 'is-invalid' : ''"
+          id="username"
+          type="text"
+          placeholder="Ф.И.О."
+          required></v-input>
+          <v-input
+          v-model.trim="form.password"
+          :class="$v.form.password.$error ? 'is-invalid' : ''"
+          id="password"
+          type="password"
+          placeholder="Пароль"
+          required></v-input>
+          <p>
+            <NuxtLink to="#">Не помню пароль</NuxtLink>
+          </p>
+          <div class="buttons-wrapper">
+            <button
+            id="login"
+            class="filled">
+              Войти
+            </button>
+            <button
+            id="signup">
+              Зарегистрироваться
+            </button>
           </div>
-        </form>
-      </form-layout>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
 import VInput from '~/components/VInput'
-import FormLayout from '~/layouts/FormLayout'
 import { required, minLength, email } from 'vuelidate/lib/validators'
 
 export default {
   name: 'LoginLayout.vue',
   components: {
-    VInput,
-    FormLayout
+    VInput
   },
   data() {
     return {
@@ -91,8 +87,49 @@ export default {
   .flex(center, center);
 }
 
+#login-form {
+  width: 516px;
+}
+
+h1 {
+  text-align: center;
+}
+
+#form-inner {
+  margin-top: 48px;
+
+  & p {
+    margin-top: 8px;
+  }
+}
+
 input {
   height: 70px;
+
+  font-size: @fz-large;
+  line-height: 160%;
+}
+
+button {
+  height: 70px;
+  width: 100%;
+  margin-top: 10px;
+
+  color: @blue;
+  border: 1px solid @blue;
+
+  &:first-child {
+    margin-top: 32px;
+  }
+  &.filled {
+    background-color: @blue;
+    border: none;
+    color: @theme-color;
+  }
+}
+
+p {
+  margin-top: 18px;
 }
 
 </style>
