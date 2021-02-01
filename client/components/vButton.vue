b41d5a377506f765f88e85db9f96a30a5960cd63
Catolly <45347427+Catolly@users.noreply.github.com>
2021-02-01 22:39:10 +0700

    feat(v-button): добавить hover

--- client/components/vButton.vue
+++ client/components/vButton.vue
@@ -39,8 +39,14 @@ export default {
 .red,
 .green,
 .blue {
+	&:hover {
+		cursor: pointer;
+	}
 	&.filled {
 		border: none;
+		color: #fff;
+	}
+	&.filled:hover {
 		color: #fff;
 	}
 }
@@ -48,22 +54,43 @@ export default {
 .red {
 	color: @red;
 	border: 1px solid @red;
+	&:hover {
+		color: @red-hover;
+		border-color: @red-hover;
+	}
 	&.filled {
 		background-color: @red;
+	}
+	&.filled:hover {
+		background-color: @red-hover;
 	}
 }
 .green {
 	color: @green;
 	border: 1px solid @green;
+	&:hover {
+		color: @green-hover;
+		border-color: @green-hover;
+	}
 	&.filled {
 		background-color: @green;
+	}
+	&.filled:hover {
+		background-color: @green-hover;
 	}
 }
 .blue {
 	color: @blue;
 	border: 1px solid @blue;
+	&:hover {
+		color: @blue-hover;
+		border-color: @blue-hover;
+	}
 	&.filled {
 		background-color: @blue;
+	}
+	&.filled:hover {
+		background-color: @blue-hover;
 	}
 }
 
