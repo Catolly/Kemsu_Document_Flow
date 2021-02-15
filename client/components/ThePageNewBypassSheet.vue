25ac7640a91b204650ed51ac497eab167c4a4f3c
Catolly <45347427+Catolly@users.noreply.github.com>
2021-02-15 13:48:57 +0700

    feat(NewBypassSheet page): создать страницу создания заявления на обходной лист

---
+++ client/components/ThePageNewBypassSheet.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="container">
+		<h1 class="header">Обходной лист</h1>
+	</div>
+</template>
+
+<script>
+import VButton from '~/components/VButton'
+
+export default {
+	name: 'ThePageNewBypassSheet',
+	components: {
+		VButton,
+	},
+	methods: {
+	},
+	data() {
+		return {
+
+		}
+	}
+}
+</script>
+
+<style lang="less" scoped>
+
+.header {
+	padding-top: 48px;
+}
+
+</style>
---
+++ client/pages/New-Bypass-Sheet.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="container">
+		<the-page-new-bypass-sheet />
+  </div>
+</template>
+
+<script>
+import ThePageNewBypassSheet from '~/components/ThePageNewBypassSheet'
+
+export default {
+  name: 'NewBypassSheet',
+  components: {
+    ThePageNewBypassSheet,
+  },
+}
+</script>
+
+<style>
+
+</style>
