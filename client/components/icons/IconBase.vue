1d36cb4fe31b61011590797e956f6f54383d1e52
Catolly <45347427+Catolly@users.noreply.github.com>
2021-02-26 11:56:24 +0700

    feat(IconBase, arrow icons): создать компонент для svg стрелок

---
+++ client/components/icons/IconBase.vue
@@ -0,0 +1,27 @@
+<template>
+	<svg 
+	:width="width" 
+	:height="height" 
+	viewBox="0 0 24 16"
+	class="icon-base" 
+	xmlns="http://www.w3.org/2000/svg">
+		<slot />
+	</svg>
+</template>
+
+<script>
+
+export default {
+	name: 'IconBase',
+	props: {
+		width: {
+			type: [Number, String],
+			default: 16
+		},
+		height: {
+			type: [Number, String],
+			default: 16
+		},
+	}
+}
+</script>
