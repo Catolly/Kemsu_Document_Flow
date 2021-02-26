9f77e4675eced4ccc4d8d71cfbb0f4e57c3e7726
Catolly <45347427+Catolly@users.noreply.github.com>
2021-02-26 12:09:02 +0700

    refact(ThePageBypassSheet): изменить аргумент в openBypassSheet

--- client/components/ThePageBypassSheet.vue
+++ client/components/ThePageBypassSheet.vue
@@ -36,7 +36,7 @@ export default {
 	},
 	methods: {
 		openBypassSheet(bypassSheet) {
-			this.$router.push('/bypass-sheet/' + bypassSheet)
+			this.$router.push('/bypass-sheets/' + bypassSheet.id)
 		},
 		openAddStatementForm() {
 			this.$router.push('/new-bypass-sheet')
