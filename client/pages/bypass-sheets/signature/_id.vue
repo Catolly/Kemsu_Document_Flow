40d5d3213eb5e3cc77c1c67fd0b1b390166b1a0a
Catolly <45347427+Catolly@users.noreply.github.com>
2021-04-12 03:04:47 +0700

    fix(signature page): fix layout

--- client/pages/bypass-sheets/signature/_id.vue
+++ client/pages/bypass-sheets/signature/_id.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="container">
-    <h1 class="header">
-      {{title}}
-      <app-group-nav />
-      <div class="topbar">
-        <app-button class="btn green filled">Подписать (3)</app-button>
-        <app-button class="btn">Выбрать всех</app-button>
-        <app-button class="btn">Снять выбор</app-button>
-        <app-input id="input" class="input round search" />
-      </div>
-      <app-list	class="student-list">
-        <app-list-item>
-        </app-list-item>
-      </app-list>
-    </h1>
+    <h1 class="header">{{title}}</h1>
+    <app-group-nav class="app-group-nav mt-4" />
+    <div class="topbar">
+      <app-button class="btn green filled">Подписать (3)</app-button>
+      <app-button class="btn">Выбрать всех</app-button>
+      <app-button class="btn">Снять выбор</app-button>
+      <app-input id="input" class="input round search" />
+    </div>
+    <app-list	class="student-list">
+      <app-list-item>
+      </app-list-item>
+    </app-list>
   </div>
 </template>
 
@@ -45,6 +43,10 @@ export default {
 .header {
   margin-top: 48px;
 }
+
+// .app-group-nav {
+//   margin-top: 16px;
+// }
 
 .topbar {
   margin-top: 48px;
