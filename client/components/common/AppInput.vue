b57d726c4a369c2eb2cb29869c3b338f747a902c
Catolly <45347427+Catolly@users.noreply.github.com>
2021-04-15 19:05:34 +0700

    fix(AppInput, style scope): исправить протекание стилей

--- client/components/common/AppInput.vue
+++ client/components/common/AppInput.vue
@@ -49,16 +49,6 @@ export default {
 <style lang="less" scoped>
 @input-background: #FDFDFD;
 
-.label,
-.app-input {
-	transition: .2s ease all;
-}
-
-.app-input, // need to not hide parent corners
-.app-input-wrapper {
-  border-radius: 10px;
-}
-
 .app-input-wrapper {
 	position: relative;
 
@@ -66,51 +56,61 @@ export default {
   width: 100%;
 
   border: 1px solid #F3F3F3;
-  background: @input-background;
 
   &.round,
   &.round .app-input {
     border-radius: 100px;
   }
-}
 
-.app-input {
-  position: relative;
+  &,
+  .app-input {
+    border-radius: 10px;
+  }
 
-  min-height: 100%;
-  min-width: 100%;
-  padding-left: 24px;
+  .label,
+  .app-input {
+    transition: .2s ease all;
+  }
 
-  border: 1px solid transparent;
+  .app-input {
+    position: relative;
 
-  &:focus {
-  	border-color: @blue;
-    & + .label {
-    	color: @blue;
+    min-height: 100%;
+    min-width: 100%;
+    padding-left: 24px;
+
+    border: 1px solid transparent;
+    background: @input-background;
+
+    &:focus {
+      border-color: @blue;
+      & + .label {
+        color: @blue;
+      }
+    }
+
+    // Фиксирует плейсхолдер на верхней левой границе при фокусе
+    &:focus,
+    &:not(:placeholder-shown) {
+      & + .label {
+        font-size: @fz-small;
+        top: -0.875em;
+        left: 1.5em;
+        padding: 0.25em 0.5em;
+      }
     }
   }
 
-  // fix placeholder on input top border
-  &:focus,
-  &:not(:placeholder-shown) {
-    & + .label {
-  	  font-size: @fz-small;
-  	  top: -0.875em;
-  	  left: 1.5em;
-  	  padding: 0.25em 0.5em;
-    }
+  .label {
+    position: absolute;
+    top: calc(50% - .6em); // Фиксирует label по центру input'а
+    left: 1.2em;
+
+    color: @text-grey;
+    background: linear-gradient(to top, @input-background 50%, transparent 0);
+
+    user-select: none;
   }
-}
-
-.label {
-	position: absolute;
-  top: calc(50% - .6em); // fix on input center
-  left: 1.2em;
-
-	color: @text-grey;
-	background: linear-gradient(to top, @input-background 50%, transparent 0);
-
-  user-select: none;
 }
 
 </style>
