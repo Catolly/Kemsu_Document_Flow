30f451ced7dfd11501512129a5b874e885615321
Catolly <45347427+Catolly@users.noreply.github.com>
2021-04-22 23:29:58 +0700

    feat(chips): добавить компонент чипса

---
+++ client/components/common/AppChips.vue
@@ -0,0 +1,31 @@
+<template>
+  <span
+    class="app-chips"
+    @click="$emit('click')"
+  >
+    <slot />
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'AppChips'
+}
+</script>
+
+<style lang="less" scoped>
+
+.app-chips {
+  display: inline-block;
+  box-sizing: border-box;
+
+  padding: 12px 24px;
+  width: fit-content;
+
+  font-size: @fz-small;
+  color: @blue;
+  border: 1px solid @blue;
+  border-radius: 100px;
+}
+
+</style>
