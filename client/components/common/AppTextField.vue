2de864e53c4a9f2e6a930b6d08477995596dc9bb
Catolly <45347427+Catolly@users.noreply.github.com>
2021-04-22 23:50:40 +0700

    feat(AppTextField): создать компонент тектового поля

---
+++ client/components/common/AppTextField.vue
@@ -0,0 +1,26 @@
+<template>
+  <textarea
+    rows="7"
+    class="app-text-field"
+  />
+</template>
+
+<script>
+export default {
+  name: 'AppTextField',
+}
+</script>
+
+<style lang="less" scoped>
+
+.app-text-field {
+  padding: 32px;
+
+  background: @grey-bright;
+  border: 1px solid @grey-light;
+  border-radius: 5px;
+
+  resize: none;
+}
+
+</style>
