2917f18177c134ec303bd99bd270e7f911f64622
Catolly <45347427+Catolly@users.noreply.github.com>
2021-04-22 23:02:15 +0700

    feat(icon close): добавить иконку с крестиком

---
+++ client/components/icons/IconClose.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="icon-logo">
+    <icon-base>
+      <path d="M15.0299 16L8.53419 9.50427L2.2094 15.8291L0.670941 14.2906L6.99573 7.96581L0.5 1.47009L1.97009 0L8.46581 6.49573L14.8248 0.136752L16.3632 1.67521L10.0043 8.03419L16.5 14.5299L15.0299 16Z" fill="black"/>
+    </icon-base>
+  </div>
+</template>
+
+<script>
+import IconBase from '~/components/icons/IconBase'
+
+export default {
+  name: 'IconClose',
+
+  components: {
+    IconBase
+  },
+}
+</script>
