4bb92760703cc9af3d7a6e9f8891383a7eeaca5a
Catolly <45347427+Catolly@users.noreply.github.com>
2021-04-24 22:23:27 +0700

    refact(AppFormSignupStaff): изменить структуру компонента
    - Изменить стили
    - Добавить кнопку редиректа на страницу логина
    - Переименовать username в fullName

--- client/components/auth-forms/AppFormSignupStaff.vue
+++ client/components/auth-forms/AppFormSignupStaff.vue
@@ -1,55 +1,63 @@
 <template>
-	<form @submit.prevent="checkForm" id="form">
-    <h1 id="form-header">Регистрация</h1>
-    <div id="form-inner">
-      <app-autocomplete
-      v-model.trim="form.username"
-      :options="relevantUsersOptions"
-      autocomplete="off"
-      id="username"
-      class="username"
-      type="text"
-      placeholder="Ф.И.О."
-      required />
-      <app-input
-      v-model.trim="form.email"
-      id="email"
-      class="email"
-      type="email"
-      placeholder="Email"
-      required />
-      <app-autocomplete
-      v-model.trim="form.department"
-      :options="departmentsOptions"
-      autocomplete="off"
-      id="department"
-      class="department"
-      type="text"
-      placeholder="Отдел, в котором работаете"
-      required />
-      <app-input
-      v-model.trim="form.password"
-      id="password"
-      type="password"
-      placeholder="Пароль"
-      required />
-      <app-button
-      id="signup"
-      class="signup-btn blue big filled fluid">
-        Зарегистрироваться
-      </app-button>
-      <NuxtLink
-      to="/login"
-      class="login-link">
+	<form @submit.prevent="checkForm" class="form">
+
+    <h1 class="header">Регистрация</h1>
+
+    <div class="body">
+      <div class="inputs">
+        <app-autocomplete
+          v-model.trim="form.fullName"
+          :options="relevantUsersOptions"
+          autocomplete="off"
+          type="text"
+          placeholder="Ф.И.О."
+          required
+        />
+
+        <app-input
+          v-model.trim="form.email"
+          type="email"
+          placeholder="Email"
+          required
+        />
+
+        <app-autocomplete
+          v-model.trim="form.department"
+          :options="departmentsOptions"
+          autocomplete="off"
+          type="text"
+          placeholder="Отдел, в котором работаете"
+          required
+        />
+
+        <app-input
+          v-model.trim="form.password"
+          type="password"
+          placeholder="Пароль"
+          required
+        />
+      </div>
+
+      <div class="btns">
         <app-button
-        class="login-btn blue big fluid">
+          class="signup-btn blue big filled fluid"
+        >
+          Зарегистрироваться
+        </app-button>
+
+        <app-button
+          class="to-login-btn blue big fluid"
+          @click="toLogin"
+        >
           Войти
         </app-button>
-      </NuxtLink>
+      </div>
+
       <p class="signup-problem">
         Кто-то зарегистрировался под вашим именем? Обратитесь к <NuxtLink to="#">администрации</NuxtLink>
       </p>
     </div>
+
   </form>
 </template>
 
@@ -60,19 +68,21 @@ import AppAutocomplete from '~/components/common/AppAutocomplete'
 
 export default {
   name: 'FormSingupStaff',
+
   components: {
     AppButton,
     AppInput,
     AppAutocomplete,
   },
-  data() {
-    return {
+
+  data:() => ({
       form: {
-        username: '',
+        fullName: '',
         email: '',
         department: '',
         password: '',
       },
+
       relevantUsers: [
         {
           id: 34,
@@ -95,6 +105,7 @@ export default {
           group: "M-174",
         },
       ],
+
       departments: [
         {
           id: 34,
@@ -117,78 +128,73 @@ export default {
           group: "M-174",
         },
       ],
-    }
-  },
+  }),
+
   computed: {
     relevantUsersOptions() {
-      return this.relevantUsers.map(user => {
-        return {
-          id: user.id,
-          value: user.fullName + ' - ' + user.group
-        }
-      })
+      return this.relevantUsers.map(user => ({
+        id: user.id,
+        value: user.fullName + ' - ' + user.group,
+      }))
     },
+
     departmentsOptions() {
-      return this.departments.map(department => {
-        return {
+      return this.departments.map(department => ({
           id: department.id,
-          value: department.fullName + ' - ' + department.group
-        }
-      })
-    }
+          value: department.fullName + ' - ' + department.group,
+      }))
+    },
   },
+
   methods: {
     checkForm() {
       // Проверка данных формы
       // ...
       this.signup()
     },
+
     async signup() {
       // Отправка данных на сервер
       // ...
 
       this.$router.push('/')
     },
-  }
+
+    toLogin() {
+      this.$router.push('/login')
+    },
+  },
 }
 </script>
 
 <style lang="less" scoped>
-@import '~/styles/index.less';
 
-#form {
-  width: 516px;
+.inputs,
+.btns {
+  display: grid;
 }
 
-#form-header {
-  text-align: center;
-}
-
-#form-inner {
-  margin-top: 48px;
-
-  .username {
-    width: 100%;
+.form {
+  .header {
+    text-align: center;
   }
 
-  .forgot-password {
-    margin-top: 8px;
-  }
+  .body {
+    margin-top: 48px;
 
-  .signup-btn {
-    margin-top: 32px;
-  }
+    .inputs {
+      grid-row-gap: 16px;
+    }
 
-  .login-btn {
-    margin-top: 10px;
-  }
+    .btns {
+      margin-top: 32px;
 
-  .login-link:after {
-    display: none;
-  }
+      grid-row-gap: 8px;
+    }
 
-  .signup-problem {
-    margin-top: 16px;
+    .signup-problem {
+      margin-top: 16px;
+    }
   }
 }
 
