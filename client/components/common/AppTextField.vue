5301f48936f203842ee6b4c2d17db307a48d6a8e
Catolly <45347427+Catolly@users.noreply.github.com>
2021-04-27 16:13:11 +0700

    refact(AppTextField): изменить стили

--- client/components/common/AppTextField.vue
+++ client/components/common/AppTextField.vue
@@ -1,26 +1,56 @@
 <template>
-  <textarea
-    rows="7"
-    class="app-text-field"
-  />
+  <div
+    class="app-text-field-wrapper"
+  >
+    <textarea
+      class="app-text-field"
+      :rows="rows"
+    />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'AppTextField',
+  props: {
+    rows: {
+      type: Number,
+      default: 7,
+    },
+  },
 }
 </script>
 
 <style lang="less" scoped>
 
-.app-text-field {
-  padding: 32px;
+.app-text-field-wrapper {
+  width: 100%;
 
-  background: @grey-bright;
-  border: 1px solid @grey-light;
-  border-radius: 5px;
+  &.error {
+    .app-text-field,
+    .app-text-field:focus {
+      border-color: @red;
+    }
+  }
 
-  resize: none;
+  .app-text-field {
+    padding: 32px;
+
+    width: inherit;
+
+    background: @grey-bright;
+    border: 1px solid @grey-light;
+    border-radius: 5px;
+
+    transition: .2s ease all;
+
+    resize: none;
+
+    &:focus {
+      border-color: @blue;
+    }
+  }
 }
+
 
 </style>
