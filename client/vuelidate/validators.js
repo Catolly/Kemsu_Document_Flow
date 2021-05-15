d07fb13bb22acfe813afc76eaa265a3ad16654d5
Catolly <45347427+Catolly@users.noreply.github.com>
2021-05-15 23:34:17 +0700

    feat(validators): удалить валидатор

--- client/vuelidate/validators.js
+++ client/vuelidate/validators.js
@@ -1,5 +1,1 @@
-export const optionExist = (value, vm) => {
-  return !!vm.relevantUsersOptionList.filter(option => option.value === value).length
-}
-
 export const twoOrThreeWordsReg = /^[а-яА-Я]+\s[а-яА-Я]+\s?[а-яА-Я]*$/ // От 2 до 3 слов
