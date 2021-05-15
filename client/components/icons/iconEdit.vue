a001a2593c2a1a87e371f9515e3674c52d344864
Catolly <45347427+Catolly@users.noreply.github.com>
2021-05-15 23:17:27 +0700

    feat(iconEdit): добавить иконку редактирования

---
+++ client/components/icons/iconEdit.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="icon-edit">
+    <icon-base
+      :height="height"
+      :width="width"
+    >
+      <path d="M15.7895 0.729492L13.2484 3.27061L17.4589 7.48114L20 4.94002L15.7895 0.729492ZM11.6591 4.85984L0 16.519V20.7295H4.21053L15.8697 9.07036L11.6591 4.85984Z" fill=""/>
+    </icon-base>
+  </div>
+</template>
+
+<script>
+import IconBase from '~/components/icons/IconBase'
+
+export default {
+  name: 'IconEdit',
+
+  components: {
+    IconBase,
+  },
+
+  props: {
+    height: {
+      type: Number,
+      default: 21,
+    },
+    width: {
+      type: Number,
+      default: 20,
+    },
+  },
+}
+</script>
