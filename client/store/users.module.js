bbab2577e8d6a98e9206fdf513884ef86d10f052
Catolly <45347427+Catolly@users.noreply.github.com>
2021-05-19 00:20:50 +0700

    feat(users ban/unban): Создать запросы бана и разбрана пользователя

--- client/store/users.module.js
+++ client/store/users.module.js
@@ -1,10 +1,9 @@
-import { UsersService } from '~/services/ApiService'
-// import Role from '~/services/role'
+import ApiService, { UsersService } from '~/services/ApiService'
 import {
   CHECK_AUTH,
-  // CHECK_ROLE,
   FETCH_USERS,
-  // UPDATE_BYPASS_SHEETS
+  BAN_USER,
+  UNBAN_USER,
   WAIT_FOR,
 } from './actions.type'
 import { SET_USERS, SET_ERROR } from './mutations.type'
@@ -27,9 +26,6 @@ const actions = {
       offset='',
       limit='',
     }) {
-    if (users.length !== 0) {
-      return context.commit(SET_USERS, users)
-    }
     return new Promise((resolve, reject) => {
       context.dispatch(WAIT_FOR, 'checkingAuth')
         .then(() => {
@@ -49,6 +45,22 @@ const actions = {
             })
         })
     })
+  },
+  async [BAN_USER](context, id) {
+    try {
+      await ApiService.post('ban/', id)
+    } catch (error) {
+      context.commit(SET_ERROR, error)
+      throw error
+    }
+  },
+  async [UNBAN_USER](context, id) {
+    try {
+      await ApiService.post('unban/', id)
+    } catch (error) {
+      context.commit(SET_ERROR, error)
+      throw error
+    }
   },
 }
 
