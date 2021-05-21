6faf942c6a2dd1fd0afc31937d5a48be606bd0ac
Catolly <45347427+Catolly@users.noreply.github.com>
2021-05-21 11:49:44 +0700

    fix(middlewares): выполнять middleware на стороне клиента

--- client/middleware/authenticated.js
+++ client/middleware/authenticated.js
@@ -1,7 +1,9 @@
 import { CHECK_AUTH } from '~/store/actions.type'
 
 export default async function ({ store, redirect }) {
-  await store.dispatch(CHECK_AUTH)
-  if (!store.state.auth.isAuthenticated)
-    return redirect('/login')
+  if (process.browser) {
+    await store.dispatch(CHECK_AUTH)
+    if (!store.state.auth.isAuthenticated)
+      return redirect('/login')
+  }
 }
