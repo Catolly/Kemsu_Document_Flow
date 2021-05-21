d25ee1679b4490c203177aa3f4d56517ae5167e7
Catolly <45347427+Catolly@users.noreply.github.com>
2021-05-21 15:17:10 +0700

    feat(AppBypassSheetListStudent): подключить константы статусов шаблона

--- client/components/bypass-sheets/AppBypassSheetListStudent.vue
+++ client/components/bypass-sheets/AppBypassSheetListStudent.vue
@@ -2,10 +2,7 @@
 	<div class="container">
 		<h1 class="header">Обходной лист</h1>
 
-    <p
-      v-if="loadError"
-      class="error-message"
-    >
+    <p v-if="loadError" class="error-message">
       Не удалось загрузить обходные листы
     </p>
 
@@ -18,23 +15,28 @@
   		</app-button>
 
       <app-pagination
-        v-if="bypassSheets.length > 0"
-        :itemsAmount="bypassSheets.length"
+        v-if="sheets.length > 0"
+        :itemsAmount="sheets.length"
         :page="page"
         @updateItemsPerPage="itemsPerPage = $event"
         @updatePage="page = $event"
       />
     </div>
 
+    <h2 v-if="!sheets.length && !loadError" class="empty-message">
+      Нет обходных листов
+    </h2>
+
 		<app-list	class="bypass-sheet-list">
 			<app-list-item
-  			v-for="(bypassSheet, index) in bypassSheetsInPage"
+  			v-for="(sheet, index) in sheetsInPage"
   			:key="index"
-  			@click="$router.push('/bypass-sheets/' + bypassSheet.id)"
+  			@click="$router.push('/bypass-sheets/' + sheet.id)"
+        :class="{'signed': sheet.status === bypassSheetSchema.Signed}"
   			class="bypass-sheet"
       >
 				<div class="bypass-sheet-title">
-					{{ bypassSheet.title }}
+					{{ sheet.title }}
 				</div>
 			</app-list-item>
 		</app-list>
@@ -42,6 +44,8 @@
 </template>
 
 <script>
+import bypassSheetSchema from '~/services/bypassSheetSchema'
+
 import { mapGetters } from "vuex"
 import { FETCH_BYPASS_SHEETS } from "~/store/actions.type"
 
@@ -70,19 +74,28 @@ export default {
   computed: {
     ...mapGetters(['bypassSheets']),
 
-    bypassSheetsInPage() {
-      return this.bypassSheets
+    sheetsInPage() {
+      return this.sheets
       .slice(
         this.page * this.itemsPerPage,
         (this.page + 1) * this.itemsPerPage
       )
+    },
+
+    sheets() {
+      return this.bypassSheets
+      .slice()
+      .reverse()
+    },
+
+    bypassSheetSchema() {
+      return bypassSheetSchema
     },
   },
 
   beforeMount() {
     this.$store
       .dispatch(FETCH_BYPASS_SHEETS, {
-        bypassSheets: this.bypassSheets,
         // department: 'ИФН' //currentUser.department
       })
       .catch(error => {
