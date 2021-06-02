3f394a44821b7df3d0c852153a76c62c26dac94a
Catolly <45347427+Catolly@users.noreply.github.com>
2021-06-03 00:00:06 +0700

    feat(about page): создать страницу "о проекте"

---
+++ client/pages/about.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="about">
+    <h1 class="header">О проекте</h1>
+
+    <h3 class="description">
+      Проект выполнен в рамках дипломной работы с целью упростить процесс прохождения обходного листа и перенести его в дистанционный формат
+    </h3>
+
+    <div class="creators">
+      <div v-for="(creator, index) in creators" :key="index" class="creator">
+        <span class="role">{{creator.role}}</span>
+        <h2 class="name">{{creator.name}}</h2>
+        <template v-for="(link, index) in creator.links">
+          <NuxtLink :key="index" :to="link.path" class="link clear">
+            <span class="line" />{{link.name}}
+          </NuxtLink>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+
+  middleware: 'authenticated',
+
+  data:() => ({
+    creators: [
+      {
+        name: 'Бурмин Леонид',
+        role: 'Научный руководитель',
+      },
+      {
+        name: 'Сергиенко Анатолий',
+        role: 'Frontend-разработчик',
+        links: [
+          {
+            name: 'Github',
+            path: 'https://github.com/Catolly',
+          },
+        ],
+      },
+      {
+        name: 'Люкшин Михаил',
+        role: 'Backend-разработчик',
+        links: [
+          {
+            name: 'Github',
+            path: 'https://github.com/gumart',
+          },
+        ],
+      },
+      {
+        name: 'Панчук Роман',
+        role: 'Backend-разработчик',
+        links: [
+          {
+            name: 'Github',
+            path: 'https://github.com/PanchukRoman',
+          },
+        ],
+      },
+      {
+        name: 'Козырева Татьяна',
+        role: 'Дизайнер',
+      },
+      {
+        name: 'Колчанов Дмитрий',
+        role: 'Тестировщик',
+      },
+      {
+        name: 'Игор Миронов',
+        role: 'Тестировщик',
+      },
+    ],
+  }),
+}
+</script>
+
+<style scoped lang="less">
+.about {
+  padding-top: 48px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 3em;
+}
+
+.description {
+  max-width: 750px;
+}
+
+.creators {
+  max-width: 1000px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 300px);
+  grid-column-gap: 1em;
+  grid-row-gap: 2em;
+}
+
+.creator {
+  display: flex;
+  flex-direction: column;
+  gap: .5em;
+
+  &:first-child {
+    grid-column: 1 / -1;
+  }
+}
+
+.role {
+  font-size: @fz-small;
+  color: @grey-darkset;
+}
+
+.link {
+  width: fit-content;
+  font-size: @fz-large;
+  font-weight: @fw-normal;
+  color: @black;
+
+  display: flex;
+  align-items: center;
+}
+
+.line {
+  margin-right: .5em;
+
+  display: inline-block;
+  width: 1em;
+
+  position: relative;
+  &:before {
+    .absolute();
+    top: 50%;
+    height: 1px;
+    width: 100%;
+    background: @grey-darkset;
+  }
+}
+</style>
