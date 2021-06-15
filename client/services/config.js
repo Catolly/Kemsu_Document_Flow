6961611cda99e40aa38a33c17a237670cd8d7240
Catolly <45347427+Catolly@users.noreply.github.com>
2021-06-15 13:07:39 +0700

    refact(api config): увеличить таймаут

--- client/services/config.js
+++ client/services/config.js
@@ -2,7 +2,7 @@ export const BASE_URL = process.env.NODE_ENV === 'production'
                                               ? 'http://mydoc.kemsu.ru'
                                               : 'http://25.35.74.172:8000'
 export const API_URL = `${BASE_URL}/api/`
-export const REQUEST_TIMEOUT = 35000
+export const REQUEST_TIMEOUT = 60*1000*1000
 
 export const debounceDelay = 1500
 export const throttleDelay = 500
